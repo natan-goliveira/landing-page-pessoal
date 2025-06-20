79d4eeebc88c71e18393578da602a54e36e4108d
Natan Oliveira <natantatan2000@gmail.com>
2025-06-19 22:04:10 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//declaração de Variáveis e atribuição de valores
+let boasVindas = "Hello, World!";
+boasVindas = "Hello, Universe!";
+
+//constante não pode mudar o valor//
+const canabisIsLife = true;
+canabisIsLife = false; // Isso causará um erro, pois canabisIsLife é uma constante
+
+//String
+""
+''
+``
+
+//Number
+123
+-123
+3.14
+
+//Boolean
+true    
+false
